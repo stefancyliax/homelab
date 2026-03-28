b0f5b77289025da20a7ecbcd50096e2c516d7f44
stefancyliax <stefan.cyliax@gmail.com>
2026-03-28 23:01:38 +0100

    add action for terraform plan

---
+++ terraform/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc4"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true # Set to false if you have a valid SSL certificate
+}
+
+variable "proxmox_api_url" {
+  type      = string
+  sensitive = false
+}
+
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
