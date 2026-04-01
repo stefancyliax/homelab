3e3ef07158040fe100b5234508447e1deac2fedb
stefancyliax <stefan.cyliax@gmail.com>
2026-04-02 00:12:24 +0200

    another update

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    colmena.url = "github:zhaofengli/colmena";
+    colmena.url = "github:zhaofengli/colmena/stable";
   };
 
   outputs = { self, nixpkgs, colmena }: {
