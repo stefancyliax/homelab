dcedc8745f8d5afb50d7341543ee5341e9d4c593
stefancyliax <stefan.cyliax@gmail.com>
2026-04-01 23:53:41 +0200

    migration to nix flakes

---
+++ NixOS/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Homelab NixOS Hive";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    colmena.url = "github:zhaofengli/colmena";
+  };
+
+  outputs = { self, nixpkgs, colmena }: {
+
+    colmenaHive = colmena.lib.makeHive {
+      meta = {
+        nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+        description = "Homelab NixOS Hive";
+      };
+
+      defaults = { pkgs, ... }: {
+        deployment = {
+          targetUser = "stefan";
+          privilegeEscalationCommand = [ "sudo" "-S" "-p" "''" "--" ];
+        };
+      };
+
+      infra-stack = { name, nodes, pkgs, ... }: {
+        deployment.targetHost = "10.1.23.240";
+        imports = [ ./nodes/infra-stack/configuration.nix ];
+      };
+
+      services-stack = { name, nodes, pkgs, ... }: {
+        deployment.targetHost = "10.1.23.36";
+        imports = [ ./nodes/services-stack/configuration.nix ];
+      };
+
+      another-test = { name, nodes, pkgs, ... }: {
+        deployment.targetHost = "10.1.23.242";
+        imports = [ ./nodes/another-test/configuration.nix ];
+      };
+    };
+
+  devShells.default = pkgs.mkShell {
+    packages = with pkgs; [
+      colmena
+      ];
+    };
+  # Optional: nix develop to get colmena in your shell
+  devShells.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
+    packages = [ colmena.packages.aarch64-darwin.colmena ];    
+  };
+};
--- NixOS/hive.nix
+++ NixOS/hive.nix
@@ -1,36 +1,36 @@
-{
-  meta = {
-    nixpkgs = <nixpkgs>;
-    description = "Homelab NixOS Hive";
-  };
+# {
+#   meta = {
+#     nixpkgs = <nixpkgs>;
+#     description = "Homelab NixOS Hive";
+#   };
 
-  defaults = { pkgs, ... }: {
-    deployment = {
-      targetUser = "stefan"; # Using your user as defined in configuration.nix
-      # Allow using sudo for deployment
-      privilegeEscalationCommand = [ "sudo" "-S" "-p" "''" "--" ];
-    };
-  };
+#   defaults = { pkgs, ... }: {
+#     deployment = {
+#       targetUser = "stefan"; # Using your user as defined in configuration.nix
+#       # Allow using sudo for deployment
+#       privilegeEscalationCommand = [ "sudo" "-S" "-p" "''" "--" ];
+#     };
+#   };
 
-  infra-stack = { name, nodes, pkgs, ... }: {
-    deployment = {
-      targetHost = "10.1.23.240";
-    };
-    imports = [ ./nodes/infra-stack/configuration.nix ];
-  };
+#   infra-stack = { name, nodes, pkgs, ... }: {
+#     deployment = {
+#       targetHost = "10.1.23.240";
+#     };
+#     imports = [ ./nodes/infra-stack/configuration.nix ];
+#   };
 
-  services-stack = { name, nodes, pkgs, ... }: {
-    deployment = {
-      targetHost = "10.1.23.36";
-    };
-    imports = [ ./nodes/services-stack/configuration.nix ];
-  };
+#   services-stack = { name, nodes, pkgs, ... }: {
+#     deployment = {
+#       targetHost = "10.1.23.36";
+#     };
+#     imports = [ ./nodes/services-stack/configuration.nix ];
+#   };
 
-  another-test = { name, nodes, pkgs, ... }: {
-    deployment = {
-      targetHost = "10.1.23.242";
-    };
-    imports = [ ./nodes/another-test/configuration.nix ];
-  };
+#   another-test = { name, nodes, pkgs, ... }: {
+#     deployment = {
+#       targetHost = "10.1.23.242";
+#     };
+#     imports = [ ./nodes/another-test/configuration.nix ];
+#   };
 
-}
+# }
