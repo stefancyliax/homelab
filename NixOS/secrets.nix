738180d31919bae69cffd287c62ad092a739a757
stefancyliax <stefan.cyliax@gmail.com>
2026-04-13 16:03:56 +0200

    feat: add Comin module and configure comin-test node with deployment keys

--- NixOS/secrets.nix
+++ NixOS/secrets.nix
@@ -7,11 +7,15 @@ let
   infra-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjYs+UlG1KAEDQSawTdliumatYyEaCfWBEMr7ksGfMC root@nixos-base";
   services-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINRRVU8zF8sW1JZhed7j4BszcAuUpEalL+nr0ZWOntfA root@nixos-base";
   another-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7NtsOzf6BjKWZUiNFYONrm16K9GGPrtD/Z30cCqOs+ root@nixos-base";
+  comin-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVZbdM/BB07b5K277nQtAgRQTpSOJJ2/pqVjd+2laT/ root@nixos-base";
   
-  systems = [ infra-node services-node another-node ]; 
+  systems = [ infra-node services-node another-node comin-test ]; 
 in
 {
   # Example: The hawser token can be decrypted by Stefan and (eventually) the nodes that run Hawser.
   "secrets/hawser-token.age".publicKeys = users ++ systems;
   "secrets/rclone-conf.age".publicKeys = users ++ [ services-node ];
+  
+  # Comin deploy key, readable by the user and all systems that might run Comin
+  "secrets/comin-deploy-key.age".publicKeys = users ++ systems;
 }
