b13643677fb3a709d220ce18b91bf3122123a8b3
stefancyliax <stefan.cyliax@gmail.com>
2026-04-14 14:37:17 +0200

    fix: naming for secret

--- NixOS/modules/comin.nix
+++ NixOS/modules/comin.nix
@@ -9,13 +9,13 @@
       name = "origin";
       url = "https://github.com/stefancyliax/homelab.git";
       branches.main.name = "main";
-      auth.access_token_path = config.age.secrets."comin-github-pat".path;
+      auth.access_token_path = config.age.secrets."github-pat".path;
     }];
   };
 
   # Decrypt the GitHub PAT using agenix
-  age.secrets."comin-github-pat" = {
-    file = ../secrets/comin-github-pat.age;
+  age.secrets."github-pat" = {
+    file = ../secrets/github-pat.age;
     owner = "root";
     group = "root";
     mode = "0400";
